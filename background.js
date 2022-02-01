da6bc8aadaff655f876e4dfe8010dc3ebff83cc5
pavelperc <pavelperc@mail.ru>
2022-02-01 15:20:52 +0300

    Initial commit

---
+++ background.js
@@ -0,0 +1,44 @@
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.contextMenus.create({
+    title: 'v-a-s-y-a',
+    id: 'vasya',
+    contexts: ['selection'],
+    documentUrlPatterns: ['http://ci.bookmate.services/*'],
+  });
+});
+
+chrome.contextMenus.onClicked.addListener(function(itemData, tab) {
+  if (itemData.menuItemId === 'vasya') {
+
+    const fixedSelection = cutTimestamps(itemData.selectionText);
+    console.log("selection:\n" + itemData.selectionText);
+    console.log("fixed selection:\n" + fixedSelection);
+
+    chrome.scripting.executeScript({
+      target: {tabId: tab.id},
+      func: copy,
+      args: [fixedSelection],
+    });
+    // chrome.tabs.getCurrent(function(tab) {
+    //   copy("abc");
+    // });
+    // copy(itemData.selectionText.replace(/[a-k]/g, 'X'));
+
+  }
+});
+
+function cutTimestamps(selection) {
+  return selection.replace(/\[[\d-T:.Z]+] /g, "\n");
+}
+
+function copy(text) {
+  const ta = document.createElement('textarea');
+  ta.style.cssText = 'opacity:0; position:fixed; width:1px; height:1px; top:0; left:0;';
+  ta.value = text;
+  document.body.appendChild(ta);
+  ta.focus();
+  ta.select();
+  document.execCommand('copy');
+  ta.remove();
+  console.log('copied abc');
+}
